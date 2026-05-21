1a0af55cce1e508b616370200eac0e534acef4f9
João Vitor Guimarães <joao_jvga@icloud.com>
2026-05-21 14:56:17 -0300

    feat: adiciona serializers

---
+++ api/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from .models import Categoria, Produto, VariacaoProduto, Pedido, ItemPedido, Carrinho, ItemCarrinho, User
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+class VariacaoProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VariacaoProduto
+        fields = '__all__'
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
+
+class ItemPedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemPedido
+        fields = '__all__'
+
+class CarrinhoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Carrinho
+        fields = '__all__'
+
+class ItemCarrinhoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemCarrinho
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'tipo', 'created_at', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
